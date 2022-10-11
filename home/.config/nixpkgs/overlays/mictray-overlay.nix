8850755725e915b7df5325b6d5691635e7bf1300
Benjamin Große <ste3ls@gmail.com>
2022-10-11 08:45:12 +0100

    mictray overlay

---
+++ home/.config/nixpkgs/overlays/mictray-overlay.nix
@@ -0,0 +1,29 @@
+with import <nixpkgs> {};
+with pkgs;
+
+self: super: {
+  mictray = super.mictray.overrideAttrs (oldAttrs: {
+    version = "0.2.5";
+    src = super.fetchFromGitHub {
+      owner = "benjajaja";
+      repo = "mictray";
+      rev = "15de891e48da9f8f9f1b8d4a3615a0d40117dca6";
+      sha256 = "sha256-3QsWqi6KOPHX62RxvAn5C0P9Sx2R8imzXawUn+ELYxI=";
+    };
+    nativeBuildInputs = [
+      meson
+      ninja
+      pkg-config
+      vala
+      wrapGAppsHook
+    ];
+
+    buildInputs = [
+      gtk3
+      libgee
+      libnotify
+      libpulseaudio
+      keybinder3
+    ];
+  });
+}
